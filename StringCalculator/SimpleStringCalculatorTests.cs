ebaa97063f9a89e35e69363ec0002c7b5e39dced
Scott MacLellan <s.j.maclell@gmail.com>
2015-09-13 22:06:40 -0400

    Add tests with duplicate behaviour

---
+++ StringCalculator/SimpleStringCalculatorTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+namespace StringCalculator {
+
+    public class SimpleStringCalculatorTests {
+
+        [Test]
+        public void Add_EmptyString_ReturnsZero() {
+            StringCalculator calculator = new StringCalculator();
+
+            int total = calculator.Add("");
+
+            Assert.AreEqual(0, total);
+        }
+
+        [Test]
+        public void Add_SingleNumber_ReturnsTheNumber() {
+            StringCalculator calculator = new StringCalculator();
+
+            int total = calculator.Add("1");
+
+            Assert.AreEqual(1, total);
+        }
+    }
+}
